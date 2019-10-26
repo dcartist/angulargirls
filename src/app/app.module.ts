9581921dda8fc90b21fb7c1ac6155984a65ed4df
DCartist <pbannerman13@gmail.com>
2019-10-26 14:31:39 -0400

    added component todo-item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { TesterComponent } from './tester/tester.component';
 import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
 import { ThisTestComponent } from './this-test/this-test.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 /*
 @NgModule({...}) is a decorator. A decorator is just a function. When using it we put @ before its name. This way it becomes a decorator: it looks at what is written right after the function call and receives it as an argument. Decorators usually do something with what they decorate. For example, in this case NgModule receives the AppModule class and adds to it methods and members that later on will be used by Angular. This way Angular will recognize that this class represents an NgModule.
 
@@ -15,7 +16,8 @@ import { ThisTestComponent } from './this-test/this-test.component';
     AppComponent,
     TesterComponent,
     InputButtonUnitComponent,
-    ThisTestComponent
+    ThisTestComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule
