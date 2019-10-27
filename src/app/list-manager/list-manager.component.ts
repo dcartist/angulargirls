b0552fbfb6403ca0fe13c48c53c541b3ef92fe5b
DCartist <pbannerman13@gmail.com>
2019-10-26 20:16:10 -0400

    updated now list

--- src/app/list-manager/list-manager.component.ts
+++ src/app/list-manager/list-manager.component.ts
@@ -17,10 +17,6 @@ import { TodoItem } from '../interfaces/todo-item';
 export class ListManagerComponent implements OnInit {
   todoList: TodoItem[] = [
     {title: 'install NodeJS'},
-    {title: 'install Angular CLI'},
-    {title: 'create new app'},
-    {title: 'serve app'},
-    {title: 'develop app'},
     {title: 'deploy app'},
   ];
 
